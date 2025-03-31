3e3e0ba6450dd681e4bef0bd97628fb6091119e4
Mike Kroutikov <mkroutikov@innodata.com>
2025-03-31 15:53:21 -0400

    registering not needed (and fails) as there are no custom modules with mistral-small

--- merge_lora_weights.py
+++ merge_lora_weights.py
@@ -4,10 +4,11 @@ from load import load_lora
 def merge_lora_and_save(model_path, save_model_path):
     processor, model = load_lora(model_path=model_path)
 
-    model.register_for_auto_class('AutoModelImageTextToText')    
+    # model.register_for_auto_class('AutoModelImageTextToText')
     model.save_pretrained(save_model_path, safe_serialization=True)
-    processor.register_for_auto_class()
+    # processor.register_for_auto_class()
     processor.save_pretrained(save_model_path, safe_serialization=True)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
